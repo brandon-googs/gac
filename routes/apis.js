f2cfce03ed966790f33af1704a4bd4de4d15d0a7
brandon clark <brandonclark@brandons-mbp.attlocal.net>
2017-09-11 22:26:06 -0500

    added with redis cloud

--- routes/apis.js
+++ routes/apis.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var redis = require("redis");
+var client = redis.createClient( 17739,  'redis-17739.c1.us-central1-2.gce.cloud.redislabs.com' );
+
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Search Suggestions Demo' });
+router.get('/autocomplete/:term', function(req, res, next) {
+    console.log("query: " + req.param("term"));
+    var args2 = ["movie", "[The", '(The\xff', 'LIMIT', "0", "2"];
+    client.zrangebylex('products2', '[' + req.param("term"), '[' + req.param("term") + '\xff', 'LIMIT', "0", "10", 
+    function(err, reply){
+    if (err !== null){
+      console.log("error: " + err);
+        } else {
+        var str = JSON.stringify(reply.sort());
+        console.log(str);
+        res.send(str, 200)
+        }
+    }); 
 });
 
 module.exports = router;
